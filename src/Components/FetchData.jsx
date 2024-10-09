94521ee5911b4227bf5a2d186c3bfba6be1fec47
enepac <enrico.acha@gmail.com>
2024-10-08 21:34:33 -0400

    Retrieve Data in Front End

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -7,7 +7,20 @@ const FetchData = () => {
   return (
     <>
      <ul className='list_data_main'>
-        <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
+     <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
+     {data && data.map((e)=>(
+        <>
+        <li className='list_data'>
+            <h3>{e.name}</h3>
+            <p><strong>Importance: </strong>{e.importance}</p>
+            <p><strong>Benefits: </strong>{e.benefits}</p>
+            <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+
+        </li>
+        </>
+        ))}
+
+        
      </ul>
     </>
   )
